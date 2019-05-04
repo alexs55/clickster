ee256decdf74b3ba4d413dbed360e0e980919cb9
alexs55 <alex.acosta.dv@gmail.com>
2019-05-04 11:35:57 -0500

    added pictures for cards to be populated

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+    return (
+        <nav class="navbar"><ul><li class="brand"><a href="/">Clicky Game</a></li><li class="">You guessed incorrectly!</li><li>Score: 0 | Top Score: 6</li></ul></nav>
+    );
+}
+
+export default Header;
+
+
+
+
+
+
