a5aea093627b9bd6f2031aedb6c7051f2b24aff7
alexs55 <alex.acosta.dv@gmail.com>
2019-05-04 19:30:03 -0500

    fixed text

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -4,10 +4,8 @@ import "./jumbotron.css";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both",  textAlign: "center", margin: 0 }}
-      className="jumbotron"
-    >
-     Click any image to start.
+      style={{ height: 330, clear: "both",  textAlign: "center", margin: 0 }}
+      className="jumbotron">
      Don't click an image more than once to increase your high score!
     </div>
   );
