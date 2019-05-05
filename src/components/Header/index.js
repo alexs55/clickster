e8201b638113853f7a75fbbb83978b470b5455be
alexs55 <alex.acosta.dv@gmail.com>
2019-05-04 19:45:31 -0500

    fixed text

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import './header.css'
 const Header = props =>(
         <nav className="header">
         <ul><li className="title">
-        <a href="/">Poke-Click</a>
+       <a href="/">Poke-Click</a>
         </li><li className="scores">Score: {props.score} | High Score: {props.highscore}</li>
         </ul>
         </nav>
